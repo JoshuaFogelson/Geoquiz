5561b5bedd2308c8e5b92f07c2cc0fe78f8dc8dc
Josh <joshfogelson@gmail.com>
2018-04-30 17:53:05 -0700

    test

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -14,6 +14,8 @@ public class QuizActivity extends AppCompatActivity {
 
     private static final String TAG = "QuizActivity";
     private static final String KEY_INDEX = "index";
+    private static final String KEY_INDEX_QUESTIONS_ANSWERED = "answered";
+    private static final String KEY_INDEX_QUESTION_CORRECT = "correct";
     private Button mTrueButton;
     private Button mFalseButton;
     private ImageButton mNextButton;
@@ -39,6 +41,19 @@ public class QuizActivity extends AppCompatActivity {
 
         if (savedInstanceState != null){
             this.mCurrentIndex= savedInstanceState.getInt(KEY_INDEX, 0);
+
+            boolean[] questionsAnswered =
+                    savedInstanceState.getBooleanArray(KEY_INDEX_QUESTIONS_ANSWERED);
+            assert questionsAnswered != null;
+            for (int i = 0; i < questionsAnswered.length; i++)
+                mQuestionBank[i].setAnswered(questionsAnswered[i]);
+
+            boolean[] questionCorrect =
+                    savedInstanceState.getBooleanArray(KEY_INDEX_QUESTION_CORRECT);
+            assert questionCorrect != null;
+            for (int i = 0; i < questionCorrect.length; i++) {
+                mQuestionBank[i].setCorrect(questionCorrect[i]);
+            }
         }
 
         this.mQuestionsTextView = (TextView) findViewById(R.id.question_text_view);
@@ -57,7 +72,6 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                checkAnswer(true);
-               mTrueButton.setEnabled(false);
             }
         });
         this.mFalseButton = (Button) findViewById(R.id.false_button);
@@ -65,7 +79,6 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 checkAnswer(false);
-                mFalseButton.setEnabled(false);
             }
         });
 
@@ -122,6 +135,18 @@ public class QuizActivity extends AppCompatActivity {
         super.onSaveInstanceState(savedInstanceState);
         Log.d(TAG,"OnSaveInstanceState");
         savedInstanceState.putInt(KEY_INDEX,mCurrentIndex);
+
+        boolean[] questionsAnswered = new boolean[mQuestionBank.length];
+        for (int i = 0; i < mQuestionBank.length; i++){
+            questionsAnswered[i] = mQuestionBank[i].isAnswered();
+        }
+        savedInstanceState.putBooleanArray(KEY_INDEX_QUESTIONS_ANSWERED, questionsAnswered);
+
+        boolean[] questionCorrect = new boolean[mQuestionBank.length];
+        for (int i = 0; i < mQuestionBank.length; i++) {
+            questionCorrect[i] = mQuestionBank[i].isCorrect();
+        }
+        savedInstanceState.putBooleanArray(KEY_INDEX_QUESTIONS_ANSWERED, questionCorrect);
     }
 
     @Override
@@ -133,21 +158,69 @@ public class QuizActivity extends AppCompatActivity {
     private void updateQuestion(){
         int question = mQuestionBank[mCurrentIndex].getTextResId();
         mQuestionsTextView.setText(question);
+
+        if (!mQuestionBank[mCurrentIndex].isAnswered()){
+            enableAnswerButtons();
+        }
+        else {
+            disableAnswerButtons();
+        }
     }
 
     private void checkAnswer(boolean userPressedTrue) {
         boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
 
+        mQuestionBank[mCurrentIndex].setAnswered(true);
+
+        disableAnswerButtons();
+
         int messageResId = 0;
 
         if (userPressedTrue == answerIsTrue) {
             messageResId = R.string.correct_toast;
+            mQuestionBank[mCurrentIndex].setCorrect(true);
         } else {
             messageResId = R.string.incorrect_toast;
+            mQuestionBank[mCurrentIndex].setCorrect(false);
         }
 
         Toast toast = Toast.makeText(this, messageResId,Toast.LENGTH_SHORT);
         toast.setGravity(Gravity.TOP,0,0);
         toast.show();
+
+        int answered = 0;
+        for (int i = 0; i < mQuestionBank.length && mQuestionBank[i].isAnswered(); i++) {
+           answered++;
+        }
+
+        String grade = new String();
+        grade = gradeTest()+"%";
+        if (answered == mQuestionBank.length){
+            Toast toast1 = Toast.makeText(this, grade,Toast.LENGTH_LONG);
+            toast1.setGravity(Gravity.CENTER,0,0);
+            toast1.show();
+        }
+    }
+
+    private void disableAnswerButtons(){
+        this.mTrueButton.setEnabled(false);
+        this.mFalseButton.setEnabled(false);
+    }
+
+    private void enableAnswerButtons(){
+        this.mTrueButton.setEnabled(true);
+        this.mFalseButton.setEnabled(true);
+    }
+
+    private int gradeTest(){
+        int numCorrect = 0;
+        for (int i = 0; i < mQuestionBank.length; i++) {
+            if (mQuestionBank[i].isCorrect()){
+                numCorrect++;
+            }
+        }
+        double doublePercent = ((double)numCorrect/(double)mQuestionBank.length)*100;
+        int percent = (int)doublePercent;
+        return percent;
     }
 }
